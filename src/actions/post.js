355479ead7d57771c09b4583a6a5c3a154094d74
mberard <mathieu@smartfire.pro>
2023-01-04 23:42:54 +0100

    fix: remove console log and add Readme file

--- src/actions/post.js
+++ src/actions/post.js
@@ -3,7 +3,6 @@ const vinted = require("../vinted-api");
 
 const getPost = async (req, res) => {
     const result = await vinted.search('https://www.vinted.fr/api/v2/catalog/items?order=newest_first&search_text=paraboot&page=1&per_page=1')
-    console.log(result)
     if (result === undefined) {
         throw new PostNotFound('Impossible de trouver le dernier post')
     }
