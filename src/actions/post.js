8462992c73cc48d48fe2e6e1bba46f0035d72028
mberard <mathieu@smartfire.pro>
2023-01-13 18:40:50 +0100

    feat: Add custom serch + styles

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,8 +1,13 @@
-const { PostNotFound } = require("../errors/PostNotFound")
-const vinted = require("../vinted-api");
+const { PostNotFound } = require('../errors/PostNotFound')
+const { convertToSlug } = require('../helper/functions')
+const vinted = require('../vinted-api')
 
 const getPost = async (req, res) => {
-    const result = await vinted.search('https://www.vinted.fr/api/v2/catalog/items?order=newest_first&search_text=paraboot&page=1&per_page=1')
+    const search = req.body
+    const searchSlug = convertToSlug(search)
+    const result = await vinted.search(
+        `https://www.vinted.fr/api/v2/catalog/items?order=newest_first&search_text=${searchSlug}&page=1&per_page=1`,
+    )
     if (result === undefined) {
         throw new PostNotFound('Impossible de trouver le dernier post')
     }
@@ -11,12 +16,10 @@ const getPost = async (req, res) => {
 }
 
 const init = async (req, res) => {
-    const post = await getPost()
-    return res.view('./templates/index.ejs', {
-        post
-    })
+    return res.view('./templates/index.ejs', { post: null })
 }
 
 module.exports = {
-    getPost, init
+    getPost,
+    init,
 }
