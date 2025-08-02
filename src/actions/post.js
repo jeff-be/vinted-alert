3c3a5ec8f5be26b74a60eec5a5e4c3a933a87e50
Mathieu Berard <mathieu.berard.web@gmail.com>
2025-08-02 23:59:17 +0200

    feat: upgrade app

--- src/actions/post.js
+++ src/actions/post.js
@@ -1,8 +1,12 @@
 const { convertToSlug } = require('../helper/functions')
 const vinted = require('../vinted-api')
 
-const getPost = async (req, res) => {
-    const search = req.body
+const getPost = async (req, reply) => {
+    const search = req.body?.query
+    if (!search || typeof search !== 'string') {
+        return reply.status(400).send({ error: 'Champ "query" requis' })
+    }
+
     const searchSlug = convertToSlug(search)
     const result = await vinted.search(
         `https://www.vinted.fr/api/v2/catalog/items?order=newest_first&search_text=${searchSlug}&page=1&per_page=1`,
